b60aff3f160e07ab0e50a173cc65f5de42f5a850
Serhii Karskyi <serhiipraktic7@gmail.com>
2025-04-04 22:16:50 +0100

    add task without slice

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,13 +1,26 @@
 import css from './ContactList.module.css';
 import Contact from '../Contact/Contact';
+import { useDispatch, useSelector } from 'react-redux';
+import { deleteContact } from '../../Redux/store';
 
-export default function ContactList({ contacts, onDelete }) {
+export default function ContactList() {
+  const dispatch = useDispatch();
+  const contact = useSelector(state => state.contacts.items);
+  const filter = useSelector(state => state.filters.name);
+
+  const filteredContacts = contact.filter(contact =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
   return (
     <>
       <ul className={css.list}>
-        {contacts.map(contact => (
+        {filteredContacts.map(contact => (
           <li className={css.item} key={contact.id}>
-            <Contact data={contact} onDelete={onDelete} />
+            <Contact
+              data={contact}
+              onDelete={id => dispatch(deleteContact(id))}
+            />
           </li>
         ))}
       </ul>
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,15 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import 'modern-normalize';
 import App from './components/App/App.jsx';
+import { store } from './Redux/store.js';
 import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <Provider store={store}>
       <App />
-    </BrowserRouter>
+    </Provider>
   </StrictMode>
 );
